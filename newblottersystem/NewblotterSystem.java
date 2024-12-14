87100c15bdbfd46991cc7e75c160f73e0eb974d8
ancline <anclineseaborg68@gmail.com>
2024-12-14 13:55:32 +0800

    Add files via upload

---
+++ newblottersystem/NewblotterSystem.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package newblottersystem;
+
+/**
+ *
+ * @author mark arwin talaba
+ */
+public class NewblotterSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       lawgin lawginFrame = new lawgin();
+       lawginFrame.setVisible(true);
+       lawginFrame.pack();
+       lawginFrame.setLocationRelativeTo(null);
+    }
+    
+}
